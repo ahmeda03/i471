81519596ebe2770d3c47ee7abf47f841dbd1a821
Ahmed Aamir <aaamir1@cs.binghamton.edu>
2025-02-17 16:58:25 -0500

    completed prj1

--- submit/prj1/prj1-sol/literals.py
+++ submit/prj1/prj1-sol/literals.py
@@ -29,12 +29,12 @@ range
  | str_range
  ;
 int_range
- : '('int'..'int')'
- | '('int'...'int')'
+ : '('INT'..'INT')'
+ | '('INT'...'INT')'
  ;
 str_range
- : '('str'..'str')'
- | '('str'...'str')'
+ : '('STR'..'STR')'
+ | '('STR'...'STR')'
  ;
 """
 
@@ -46,8 +46,7 @@ def parse(text):
         if (lookahead.kind == kind):
             lookahead = nextToken()
         else:
-            print(f'expecting {kind} at {lookahead.lexeme}',
-                  file=sys.stderr)
+            sys.stderr.write('INCORRECT TOKEN FOUND\n')
             sys.exit(1)
     def nextToken():
         nonlocal index
@@ -58,84 +57,259 @@ def parse(text):
             index += 1
             return tok
 
-    def program():
-        asts = []
+    def literals():
+        objects = []
         while (not peek('EOF')):
-            asts.append(expr())
-            consume(';')
-        return asts
-
-    def expr():
-        t = term()
-        while (peek('+') or (peek('-'))):
-            kind = lookahead.kind
-            consume(kind)
-            t1 = term()
-            t = Ast(kind, t, t1)
-        return t
+            objects.append(literal())
+        return objects
     
-    def expn():
-        t = factor()
-        if (peek('EXPN')):
-            consume('EXPN')
-            t1 = expn()
-            return Ast('**', t, t1)
-        return t
-            
-
-    def term():
-        if (peek('-')):
-            consume('-')
-            return Ast('-', term())
+    # add error handling everywhere ASAP
+    
+    def literal():
+        if (peek('NIL')):
+            return nil_token()
+        elif peek('BOOL'):
+            return bool_token()
+        elif peek('INT'):
+            return int_token()
+        elif peek('STR'):
+            return str_token()
+        elif peek('['):
+            return Array()
+        elif peek('{'):
+            return Hash()
+        elif peek('('):
+            return Range()
         else:
-            return expn()
+            sys.stderr.write("NO MATCH\n")
+            sys.exit(1)
 
-    def factor():
-        if (peek('INT')):
-            value = int(lookahead.lexeme)
+    def nil_token():
+        obj = Literal_Object('nil', None)
+        if peek('NIL'):
+            consume('NIL')
+        else:
+            sys.stderr.write('NIL TOKEN NOT FOUND\n')
+            sys.exit(1)
+        return obj
+    
+    def bool_token():
+        val = True if (lookahead.lexeme).lower() == "true" else False
+        obj = Literal_Object('bool', val)
+        if peek('BOOL'):
+            consume('BOOL')
+        else:
+            sys.stderr.write('BOOL TOKEN NOT FOUND\n')
+            sys.exit(1)
+        return obj
+
+    def int_token():
+        val = int(lookahead.lexeme)
+        obj = Literal_Object('int', val)
+        if peek('INT'):
             consume('INT')
-            ast = Ast('INT')
-            ast['value'] = value
-            return ast
         else:
+            sys.stderr.write('INT TOKEN NOT FOUND\n')
+            sys.exit(1)
+        return obj
+   
+    def str_token():
+        val = lookahead.lexeme[1:-1]
+        obj = Literal_Object('str', val)
+        if peek('STR'):
+            consume('STR')
+        else:
+            sys.stderr.write('STR TOKEN NOT FOUND\n')
+            sys.exit(1)
+        return obj
+    
+    def Array():
+        if peek('['):
+            consume('[')
+        else:
+            sys.stderr.write('OPENING BRACES NOT FOUND\n')
+            sys.exit(1)
+    
+        arr = []
+        while (not peek(']')):
+            val = literal()
+            arr.append(val)
+
+            if peek(','):
+                consume(',')
+            else:
+                sys.stderr.write('COMMA NOT FOUND\n')
+                sys.exit(1)
+
+        if peek(']'):
+            consume(']')
+        else:
+            sys.stderr.write('CLOSING BRACES NOT FOUND\n')
+            sys.exit(1)
+
+        obj = Literal_Object("array", arr)
+        return obj
+
+    def Hash():
+        if peek ('{'):
+            consume('{')
+        else:
+            sys.stderr.write('OPENING CURLY BRACES NOT FOUND\n')
+            sys.exit(1)
+        
+        hashs = []
+        while (not peek('}')):
+            hash_key = literal()
+
+            if peek('HASH_DELIMITER'):
+                consume('HASH_DELIMITER')
+            else:
+                sys.stderr.write('HASH DELIMITER NOT FOUND\n')
+                sys.exit(1)
+
+            hash_val = literal()
+
+            hashs.append({'key': hash_key, 'val': hash_val})
+
+            if peek(','):
+                consume(',')
+            else:
+                sys.stderr.write('COMMA NOT FOUND\n')
+                sys.exit(1)
+
+        if peek ('}'):
+            consume('}')
+        else:
+            sys.stderr.write('CLOSING CURLY BRACES NOT FOUND\n')
+            sys.exit(1)
+
+        obj = Literal_Object("hash", hashs)
+
+        return obj
+
+    def Range():
+        if peek('('):
             consume('(')
-            value = expr()
+        else:
+            sys.stderr.write('OPENING PARENTHESIS NOT FOUND\n')
+            sys.exit(1)
+
+        if peek('INT'):
+            return int_range()
+        elif peek('STR'):
+            return str_range()
+        else:
+            sys.stderr.write('INT OR STR TOKEN NOT FOUND\n')
+            sys.exit(1)
+
+    def int_range():
+        first_val = int_token()
+        
+        if peek('CLOSED_RANGE_DELIMITER'):
+            obj = Literal_Object('closed_range', None)
+            consume('CLOSED_RANGE_DELIMITER')
+        elif peek('HALF_OPEN_RANGE_DELIMITER'):
+            obj = Literal_Object('half_open_range', None)
+            consume('HALF_OPEN_RANGE_DELIMITER')
+        else:
+            sys.stderr.write('INCORRECT TOKEN, EXPECTING \'..\' or \'...\'\n')
+            sys.exit(1)
+
+        if peek('INT'):
+            second_val = int_token()
+        else:
+            sys.stderr.write('INT TOKEN NOT FOUND')
+            sys.exit(1)
+
+        obj['val'] = [first_val, second_val]
+
+        if peek(')'):
             consume(')')
-            return value
+        else:
+            sys.stderr.write('CLOSING PARENTHESIS NOT FOUND\n')
+            sys.exit(1)
+
+        return obj
+        
+
+    def str_range():
+        first_val = str_token()
+        
+        if peek('CLOSED_RANGE_DELIMITER'):
+            obj = Literal_Object('closed_range', None)
+            consume('CLOSED_RANGE_DELIMITER')
+        elif peek('HALF_OPEN_RANGE_DELIMITER'):
+            obj = Literal_Object('half_open_range', None)
+            consume('HALF_OPEN_RANGE_DELIMITER')
+        else:
+            sys.stderr.write('INCORRECT TOKEN, EXPECTING \'..\' or \'...\'\n')
+            sys.exit(1)
+
+        if peek('STR'):
+            second_val = str_token()
+        else:
+            sys.stderr.write('INT TOKEN NOT FOUND')
+            sys.exit(1)
+
+        obj['val'] = [first_val, second_val]
+
+        if peek(')'):
+            consume(')')
+        else:
+            sys.stderr.write('CLOSING PARENTHESIS NOT FOUND\n')
+            sys.exit(1)
+
+        return obj
 
     #begin parse()
     tokens = scan(text)
     index = 0
     lookahead = nextToken()
-    value = program()
+    val = literals()
     if (not peek('EOF')):
         print(f'expecting <EOF>, got {lookahead.lexeme}', file=sys.stderr)
         sys.exit(1)
-    return value
+    return val
 
 def scan(text):
-    SPACE_RE = re.compile(r'\s+')
+    SPACE_AND_COMMENTS_RE = re.compile(r'\s+|#.*|\n')
     NIL_RE = re.compile(r'nil')
     BOOL_RE = re.compile(r'true|false')
-    INT_RE = re.compile(r'^(\d+_)*\d+$')
-    STR_RE = re.compile(r"^'([^'\\]|\\.)*'$")
+    INT_RE = re.compile(r'(\d+_)*\d+')
+    STR_RE = re.compile(r"'([^'\\]|\\.)*'", re.S)
+    HASH_DELIMITER = re.compile(r'=>')
+    HALF_OPEN_RANGE_DELIMITER = re.compile(r'\.\.\.')
+    CLOSED_RANGE_DELIMITER = re.compile(r'\.\.')
     CHAR_RE = re.compile(r'.')
 
     def next_match(text):
-        if m := SPACE_RE.match(text):
-            return (m, None)
-        elif m := NIL_RE.match(text):
-            return (m, 'NIL')
-        elif m := BOOL_RE.match(text):
-            return (m, 'BOOL')
-        elif m := INT_RE.match(text):
-            return (m, 'INT')
-        elif m := STR_RE.match(text):
-            return (m, 'STR')
-        elif m := INT_RE.match(text):
-            return (m, 'INT')
-        elif m := CHAR_RE.match(text):  #must be last: match any char
-            return (m, m.group())
+        m = SPACE_AND_COMMENTS_RE.match(text)
+        if (m): return (m, None)
+        
+        m = NIL_RE.match(text)
+        if (m): return (m, 'NIL')
+        
+        m = BOOL_RE.match(text)
+        if (m): return (m, 'BOOL')
+
+        m = INT_RE.match(text)
+        if (m): return (m, 'INT')
+        
+        m = STR_RE.match(text)
+        if (m): return (m, 'STR')
+        
+        m = HASH_DELIMITER.match(text)
+        if (m): return (m, 'HASH_DELIMITER')
+        
+        m = HALF_OPEN_RANGE_DELIMITER.match(text)
+        if (m): return (m, 'HALF_OPEN_RANGE_DELIMITER')
+        
+        m = CLOSED_RANGE_DELIMITER.match(text)
+        if (m): return (m, 'CLOSED_RANGE_DELIMITER')
+
+        m = CHAR_RE.match(text)  #must be last: match any char
+        if (m): return (m, m.group())
+    
     tokens = []
     while (len(text) > 0):
         (match, kind) = next_match(text)
@@ -145,24 +319,12 @@ def scan(text):
     return tokens
 
 def main():
-    if (len(sys.argv) != 2): usage();
-    contents = readFile(sys.argv[1]);
-    asts = parse(contents)
-    print(json.dumps(asts, separators=(',', ':'))) #no whitespace
+    contents = sys.stdin.read()
+    result = parse(contents)
+    print(json.dumps(result, separators=(',', ':'))) #no whitespace 
 
-def readFile(path):
-    with open(path, 'r') as file:
-        content = file.read()
-    return content
-
-
-def usage():
-    print(f'usage: {sys.argv[0]} DATA_FILE')
-    sys.exit(1)
-
-#use a dict so that we can add attributes dynamically
-def Ast(tag, *kids):
-    return { 'tag': tag, } if len(kids) == 0 else { 'tag': tag, 'kids': kids }
+def Literal_Object(tag, val):
+    return { 'tag': tag, 'val': val }
 
 Token = namedtuple('Token', ['kind', 'lexeme'])
 
--- submit/prj1/prj1-sol/run.sh
+++ submit/prj1/prj1-sol/run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
+python3 ./literals.py $1
+
 # No submitted files will be executable on gradescope; ensure that
 # this script works under those conditions.
-
-
-
