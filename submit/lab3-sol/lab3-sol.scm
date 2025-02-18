844b9a2f0f9be5cd2f645527acafbd45ecd987c6
Ahmed Aamir <aaamir1@cs.binghamton.edu>
2025-02-18 12:54:31 -0500

    completed lab3

--- submit/lab3-sol/lab3-sol.scm
+++ submit/lab3-sol/lab3-sol.scm
@@ -12,7 +12,7 @@
 ;;(simple-interest balance interest-rate): return the balance
 ;;after a single simple interest payment at interest-rate %.
 (define (simple-interest balance interest-rate)
-  'TODO
+  (* balance (+ 1 (/ interest-rate 100)))
 )
 
 ;;return the amount resulting of an investment of an initial principal p,
@@ -20,12 +20,12 @@
 ;;for a total of t time periods. 
 ;;<https://www.nerdwallet.com/calculator/compound-interest-calculator>
 (define (compound-interest p r n t)
-  'TODO
+  (* p (expt (+ 1 (/ r 100 n)) (* n t)))
 )
 
 ;;return 3*x**3 + -2*x**2 + 4*x -1
 (define (poly-eval x)
-  'TODO
+  (+ -1 (+ (* 4 x) (+ (* -2 (expt x 2)) (* 3 (expt x 3)))))
 )
 
 (provide append-tails first-elements first-longest line-length)
@@ -35,14 +35,14 @@
 ;;`list2` returns the result of appending the tail of `list2` to the
 ;;tail of `list1`.
 (define (append-tails list1 list2)
-  'TODO
+  (append (cdr list1) (cdr list2))
 )
 
 ;;(first-elements list1 list2): given two non-empty lists `list1` and
 ;;`list2` returns the two-element list containing the heads of `list1`
 ;;and `list2`.
 (define (first-elements list1 list2)
-  'TODO
+  (list (car list1) (car list2))
 )
 
 ;;(first-longest list1 list2): given two non-empty lists
@@ -50,12 +50,15 @@
 ;;length is greater than that of `list2`, else it returns the first
 ;;element of `list2`.
 (define (first-longest list1 list2)
-  'TODO
+  (if (> (length list1) (length list2))
+        (car list1)
+        (car list2)
+  )
 )
 
 ;;(line-length pt0 pt1): return the length of a line between 2D-points
 ;;`pt0` and `pt1` where each point is represented as a two-element
 ;;list `(x y)` containing its coordinates `x` and `y`.
 (define (line-length pt0 pt1)
-  'TODO
+  (sqrt (+ (expt (- (car pt1) (car pt0)) 2) (expt (- (car (cdr pt1)) (car (cdr pt0))) 2)))
 )
--- submit/lab3-sol/lab3-tests.scm
+++ submit/lab3-sol/lab3-tests.scm
@@ -24,7 +24,7 @@
   (check-equal? (round-n (simple-interest 1000.0 3.56)) 1035.6)
   (check-equal? (round-n (simple-interest 1234.6 5.0)) 1296.33)
 )
-;(simple-interest-tests)
+(simple-interest-tests)
 
 
 (define (compound-interest-tests)
@@ -32,7 +32,7 @@
   (check-equal? (round-n (compound-interest 1000.0 5 12 5) 2) 1283.36)
   (check-equal? (round-n (compound-interest 1000.0 5 365 5) 2) 1284.00)
 )
-;(compound-interest-tests)
+(compound-interest-tests)
 
 
 (define (poly-eval-tests)
@@ -40,30 +40,30 @@
   (check-equal? (poly-eval 4) 175)
   (check-equal? (poly-eval 1) 4)
   (check-equal? (poly-eval 2) 23))
-;(poly-eval-tests)
+(poly-eval-tests)
 
 
 (define (append-tails-tests)
   (check-equal? (append-tails '(1 2 3) '(a b c)) '(2 3 b c))
   (check-equal? (append-tails '(1) '(a b c)) '(b c))
   (check-equal? (append-tails '(1) '(a)) '()))
-;(append-tails-tests)
+(append-tails-tests)
 
 (define (first-elements-tests)
   (check-equal? (first-elements '(1 2 3) '(a b c)) '(1 a))
   (check-equal? (first-elements '(1) '(a b c)) '(1 a))
   (check-equal? (first-elements '(1) '(a)) '(1 a)))
-;(first-elements-tests)
+(first-elements-tests)
 
 
 (define (first-longest-tests)
   (check-equal? (first-longest '(1 2 3 4) '(a b c)) 1)
   (check-equal? (first-longest '(1 2 3 ) '(a b c d)) 'a)
   (check-equal? (first-longest '(1 2 3 ) '(a b c)) 'a))
-;(first-longest-tests)
+(first-longest-tests)
 
 (define (line-length-tests)
   (check-equal? (round-n (line-length '(0.0 0.0) '(3.0 4.0))) 5.0)
   (check-equal? (round-n (line-length '(1.0 1.0) '(13.0 6.0))) 13.0)
 )
-;(line-length-tests)
+(line-length-tests)
